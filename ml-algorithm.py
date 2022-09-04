529a17977900003a35598c544afe7c5f4ca472b3
John <haywardjohn11@gmail.com>
2022-09-04 14:13:35 +0100

    Split data into training set and testing set

--- ml-algorithm.py
+++ ml-algorithm.py
@@ -20,4 +20,8 @@ url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url,names=names)
 
+array=dataset.values
+X=array[:,0:4]
+y=array[:,4]
 
+X_train, X_validation, Y_train, Y_validation = train_test_split(X,y,test_size=0.20, random_state=1)
