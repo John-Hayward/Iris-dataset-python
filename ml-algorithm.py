faea09d50a307d593a77876fcc49a26f7ebc335f
John <haywardjohn11@gmail.com>
2022-09-04 14:24:11 +0100

    Evaluated potential models and selected the SVC model before using it on dataset

--- ml-algorithm.py
+++ ml-algorithm.py
@@ -1,4 +1,6 @@
+from ast import Str
 from pickletools import read_decimalnl_short
+from random import gammavariate
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
@@ -25,3 +27,28 @@ X=array[:,0:4]
 y=array[:,4]
 
 X_train, X_validation, Y_train, Y_validation = train_test_split(X,y,test_size=0.20, random_state=1)
+
+models=[]
+models.append(('LR', LogisticRegression(solver='liblinear',multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+results=[]
+names=[]
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train,Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+    
+model=SVC(gamma='auto')
+model.fit(X_train,Y_train)
+predictions = model.predict(X_validation)
+
+print(accuracy_score(Y_validation,predictions))
+print(confusion_matrix(Y_validation,predictions))
+print(classification_report(Y_validation,predictions))
